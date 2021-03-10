104b69d42c9bbb11004fbf24d30f8edcf20d57b5
Vlad Rakut <mentos28059@gmail.com>
2021-03-10 03:33:11 +0300

    task compleated successfully

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
-
 exports.min = function min (array) {
-  return 0;
+  if(typeof array == 'undefined' || array.length == 0){
+    return 0;
+  }
+  let minNumber =array[0];
+  for(let i = 0; i < array.length; i++){
+    if(array[i] < minNumber){
+      minNumber = array[i];
+    }
+  }
+  return minNumber;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(typeof array == 'undefined' || array.length == 0){
+    return 0;
+  }
+  let maxNumber =array[0];
+  for(let i = 0; i < array.length; i++){
+    if(array[i] > maxNumber){
+      maxNumber = array[i];
+    }
+  }
+  return maxNumber;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(typeof array == 'undefined' || array.length == 0){
+    return 0;
+  }
+  let sum = 0;
+  for(let i = 0; i < array.length; i++) {
+    sum = sum + array[i];
+  }
+  return sum/array.length;
 }
